527443fba84b3a99632a838a107f3708ee6dcbca
stephonwong <stephon.wong@gmail.com>
2026-03-04 22:34:22 +1100

    move files to another repo

--- database/school_selective.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE main.school_selective (
-    school_id                    INTEGER NOT NULL,
-    selective_status             VARCHAR(100),
-    selective_status_desc        VARCHAR(100),
-    fully_selective              VARCHAR(100),
-    PRIMARY KEY (school_id)
-);
